2fff54f602e035d4565d7d1a9ffbf9aa9c5c993c
Kevin Caccamo <kevin@ciinet.org>
2019-10-02 15:28:56 -0400

    Do some housekeeping and tweaks to Space.tscn/gd
    Change the way the cockpit and camera work. The cockpit camera is now a child of the player's ship, and its transform is copied from the CockpitPosition node in Ship.tscn. The chase camera is now an InterpolatedCamera that "follows" the ChasePosition node in Ship.tscn.
    
    Add background music to Space.tscn
    
    Remove the "simple skybox" and the cubemap textures in favour of the equirectangular textures Godot wants to use for environment backgrounds
    
    Make it easy to replace the equirectangular texture used as the background

--- Space.gd
+++ Space.gd
@@ -3,24 +3,39 @@ extends Spatial
 const PlayerPilot = preload("res://PlayerPilot.gd")
 
 onready var player = $Player
-onready var camera = $Player/Camera
+onready var cockpitCamera: Camera = $Player/Camera
+onready var chaseCamera: InterpolatedCamera = $ChaseCamera
+onready var chaseView = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.controller = PlayerPilot.new()
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	# Set up chase camera
+	chaseCamera.set_target($Player/ChasePosition)
+	chaseCamera.transform = $Player/ChasePosition.transform
+	chaseCamera.enabled = true
+	chaseCamera.far = 1000
+	# Set up cockpit camera and use it as the default
+	cockpitCamera.transform = $Player/CockpitPosition.transform
+	cockpitCamera.make_current()
+	cockpitCamera.far = 1000
+	player.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Put skybox at camera position
-	var sbTransform = camera.get_global_transform()
-	$Skybox.transform.origin = sbTransform.origin
+#	var sbTransform = camera.get_global_transform()
+#	$Skybox.transform.origin = sbTransform.origin
 
 	if Input.is_action_just_pressed("toggle_chase"):
 		toggle_chasecam()
 
 func toggle_chasecam():
-	var chaseCamPos = camera.translation
-	camera.translation = $Player/Cockpit.translation
-	$Player/Cockpit.translation = chaseCamPos
-	$Player/MeshInstance.visible = not $Player/MeshInstance.visible
+	if chaseView:
+		cockpitCamera.make_current()
+	else:
+		chaseCamera.make_current()
+
+	chaseView = not chaseView
+	player.visible = not player.visible
