f08abd812b2835f4e9e7825e35656ffdc15f0d79
Kevin Caccamo <kevin@ciinet.org>
2019-10-02 15:40:19 -0400

    Add exit command

--- Fullscreen.gd
+++ Fullscreen.gd
@@ -4,3 +4,5 @@ extends Node
 func _process(delta):
 	if Input.is_action_just_pressed("win_fullscreen"):
 		OS.window_fullscreen = not OS.window_fullscreen
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
