f3dab713b7202bbbeec978149ab8a8517063c8ed
Kevin Caccamo <kevin@ciinet.org>
2019-10-03 16:57:54 -0400

    Replace camel casing with snake casing in HUD.gd

--- HUD.gd
+++ HUD.gd
@@ -4,15 +4,15 @@ const ShipControl = preload("res://Pilot.gd").ShipControl
 onready var Player = get_parent().find_node("Player")
 
 func _process(delta):
-	var setSpeed = 0
-	if Player.controlData[ShipControl.AFTERBURNER]:
-		setSpeed = Player.afterburnerSpeed
+	var set_speed = 0
+	if Player.control_data[ShipControl.AFTERBURNER]:
+		set_speed = Player.afterburner_speed
 	else:
-		setSpeed = Player.controlData[ShipControl.THROTTLE] * Player.maxSpeed
+		set_speed = Player.control_data[ShipControl.THROTTLE] * Player.max_speed
 
-	$SetContainer/SetDisplay.text = "SET: %.4f" % setSpeed
+	$SetContainer/SetDisplay.text = "SET: %.4f" % set_speed
 	$KpsContainer/KpsDisplay.text = "KPS: %.4f" % Player.velocity.length()
 
-	if Player.controlData[ShipControl.GLIDE]:
+	if Player.control_data[ShipControl.GLIDE]:
 		$SetContainer/SetDisplay.text += "G"
 		$KpsContainer/KpsDisplay.text += "G"
