efd87003d1972bfc7614ba76e2f53d9b487ee215
Kevin Caccamo <kevin@ciinet.org>
2019-10-22 16:51:48 -0400

    Add Radar to HUD, move fonts, and add Inconsolata
    Add Radar scene, associated sprites, and code
    Add Radar scene as a child of the viewport in the HUD scene
    
    Move fonts from assets/vendor/maven-pro to assets/vendor/fonts
    Add Inconsolata monospace font

---
+++ scenes/UI/Radar.gd
@@ -0,0 +1,49 @@
+extends Node2D
+# Radar - displays other ships in relation to the player
+
+var root_scene: Node
+var player: Spatial
+var size: Vector2
+const dot_image = preload("res://assets/UI/HUD/radar_dot.png")
+
+func _ready():
+	root_scene = get_tree().current_scene
+	player = root_scene.get_node("Player")
+	size = $Graphic.texture.get_size() / 2
+
+func position_for(from: Spatial, thing_pos: Vector3) -> Vector2:
+	"Get normalized 2D radar position for a 3D node from another 3D node"
+	var relative_right = from.transform.basis.xform(Vector3.RIGHT)
+	var relative_down = from.transform.basis.xform(Vector3.DOWN)
+	var relative_fwd = from.transform.basis.xform(Vector3.FORWARD)
+
+	var relative_pos = (thing_pos - from.transform.origin).normalized()
+
+	var x = relative_right.dot(relative_pos)
+	var y = relative_down.dot(relative_pos)
+	var z = relative_fwd.dot(relative_pos)
+	# Convert to spherical coordinates
+	# https://en.wikipedia.org/wiki/Spherical_coordinate_system#Coordinate_system_conversions
+	# Also https://en.wikipedia.org/wiki/Polar_coordinate_system#Converting_between_polar_and_Cartesian_coordinates
+	# var r = Vector3(x, y, z).length()  # Always 1, since relative_pos is normalized
+	var angle = atan2(y, x)
+	# r will always be 1, so there is no need to divide by r.
+	# Also, divide by PI to "normalize" the radius.
+	var distance = acos(z) / PI
+	return Vector2(distance * cos(angle), distance * sin(angle))
+
+func _process(delta):
+	for radar_dot in $Dots.get_children():
+		$Dots.remove_child(radar_dot)
+	var children = root_scene.get_children()
+	var radar_dots = []
+	for spacething in children:
+		if (spacething != player and
+				spacething.has_method("_visible_on_radar") and
+				spacething.call("_visible_on_radar")):
+			radar_dots.append(position_for(player, spacething.transform.origin) * size)
+	for radar_dot in radar_dots:
+		var dot = Sprite.new()
+		dot.position = radar_dot
+		dot.texture = dot_image
+		$Dots.add_child(dot)
