afe8bea06a3720fe0501798acd0eaa8067eec1cc
Kevin Caccamo <kevin@ciinet.org>
2019-10-21 23:50:46 -0400

    Add crosshair to HUD, and prepare to add Radar

--- scenes/UI/HUD.gd
+++ scenes/UI/HUD.gd
@@ -2,6 +2,10 @@ extends Control
 
 onready var Player = get_parent().find_node("Player")
 
+func _ready():
+	$RadarContainer/Viewport.canvas_transform = Transform2D(
+		Vector2.RIGHT, Vector2.DOWN, Vector2(64, 64))
+
 func _process(delta):
 	var set_speed = 0
 	if Player.control_data.afterburner:
