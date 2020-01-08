01cda8a09d60cacd6e547d34e2b958229a01c505
Kevin Caccamo <kevin@ciinet.org>
2020-01-08 01:03:20 -0500

    Add ShipSpawnPoint
    I had this in here for a while, but never finished or committed it

---
+++ scenes/base/ShipSpawnPoint.gd
@@ -0,0 +1,17 @@
+extends Spatial
+# Handles properly adding a ship to a mission.
+
+enum PilotType {
+	PILOT_PLAYER,
+	PILOT_AI,
+	PILOT_TURRET,
+	PILOT_CAPSHIP,
+}
+
+export(PilotType) var pilot_type
+export(PackedScene) var ship_class
+
+func _ready():
+	var ship = ship_class.instance()
+	ship.connect("weapon_fired", get_parent(), "_on_Player_weapon_fired")
+	get_parent().add_child(ship)
