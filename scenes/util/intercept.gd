0ab37a9052a5bcb30c8e6fbd788e5997d3abfaec
Kevin Caccamo <kevin@ciinet.org>
2020-02-08 00:54:11 -0500

    Improve comments in intercept.gd

--- scenes/util/intercept.gd
+++ scenes/util/intercept.gd
@@ -1,8 +1,8 @@
 extends Node
 
 # https://indyandyjones.wordpress.com/2010/04/08/intercepting-a-target-with-projectile/
-# Very important excerpt from that particular blog post:
-# Squaring and multiplication of vectors against vectors amounts to taking the dot product
+# Very important (paraphrased) excerpt from that particular blog post:
+# Squaring or multiplying two vectors means taking their dot product.
 
 # Calculate the time it will take for a projectile to hit a moving target,
 # assuming constant velocities for both the projectile and target
@@ -20,10 +20,12 @@ static func get_lead_time(to_target: Vector3,
 	# impact point would be behind the target.
 	var time = (-b - sqrt(radicand)) / (2 * a)
 	# It is more flexible to return just the impact time as opposed to the
-	# impact position.
+	# impact position. Then, it is possible to calculate the impact position
+	# like this: ImpactPos = TargetPos + (TargetVel * ImpactTime)
 	return time
 
-# Same as get_lead_time, but calculates to_target beforehand
+
+# Same as get_lead_time, but calculates to_target beforehand.
 static func find_lead_time(shot_pos: Vector3, target_pos: Vector3,
 			target_vel: Vector3, shot_speed: float) -> float:
 	var to_target = target_pos - shot_pos
